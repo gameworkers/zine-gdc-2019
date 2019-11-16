7084ef2b7ab6300d1601aa255dfc669f81bab80a
Left Burner <lefttheburneron@gmail.com>
2019-11-16 16:55:19 -0500

    Move french files to new subdirectory.

--- FrenchVersion/pandoc.sh
+++
@@ -1,7 +0,0 @@
-rm ./copy-dest/*.icml
-
-for file in copy-src/*.md; do
-    name=$(basename "$file" | cut -f 1 -d '.')
-    echo "$file -> ./copy-dest/$name.icml"
-    pandoc "$file" -s -o "./copy-dest/$name.icml"
-done
